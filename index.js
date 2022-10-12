7206043a74cba81819b3d2e9f5cca87a6effa003
gabriel <gabrielfernandes254@gmail.com>
2022-10-12 17:55:17 -0300

    feita extilizacao dos nomes, tamanho das imagens e adicionado logo

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ async function fetchPokemons(id) {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
   pokemonCard(data);
 }
 
@@ -20,17 +19,19 @@ function pokemonCard(pokemon) {
   const div = document.createElement("div");
   const title = document.createElement("h2");
   const img = document.createElement("img");
-  const imgpokemon = pokemon.sprites.other.dream_world.front_default
-  const span = document.createElement("span"); 
+  const imgpokemon = pokemon.sprites.other.dream_world.front_default;
+  const span = document.createElement("span");
 
-  div.classList.add("pokemon")
+  div.classList.add("pokemon-card");
+  img.classList.add("pokemon-img");
+  span.classList.add("id-pokemon")
 
   span.textContent = `#${pokemon.id}`;
   title.textContent = pokemon.name;
   img.setAttribute("src", `${imgpokemon}`);
 
   div.appendChild(img);
-  div.appendChild(span)
+  div.appendChild(span);
   div.appendChild(title);
   containerPokemons.appendChild(div);
 }
